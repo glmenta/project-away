dd0ef582901668b5ca5dee5cde8aeacc202563da
Gek <gakidogeck@gmail.com>
2024-09-04 12:43:26 -0700

    modified routes to test if db connected

--- app/backend/routes/userRoutes.js
+++ app/backend/routes/userRoutes.js
@@ -3,6 +3,6 @@ import { getUsers } from '../controllers/userControllers.js';
 
 const router = express.Router();
 
-router.get('/', getUsers);
+router.get('/all-users', getUsers);
 
 export default router
