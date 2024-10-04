07cdb9b76f6e41be85189b84f79f6d7f232e3726
Gek <gakidogeck@gmail.com>
2024-10-04 13:32:32 -0700

    fixed diagram route issue

--- app/backend/routes/diagramRoutes.js
+++ app/backend/routes/diagramRoutes.js
@@ -3,7 +3,7 @@ import { getDiagrams, addDiagram } from '../controllers/diagramControllers.js';
 
 const router = express.Router();
 
-router.get('/all-diagrams', getDiagrams);
+router.get('/all', getDiagrams);
 router.post('/new', addDiagram);
 
 export default router
